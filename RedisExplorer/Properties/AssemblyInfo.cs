7c47e1a2ba748a5fc1dbaea09e238cec270ba29a
Mohsen Kokabi <mohsen.kokabi@didata.com.au>
2015-05-21 16:37:42 +1000

    Changing the version from 1.0.0.0 to 0.1.0.0

--- RedisExplorer/Properties/AssemblyInfo.cs
+++ RedisExplorer/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: InternalsVisibleTo("RedisExplorer_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100830e56aa64e9ac98ba93c882cd59fe0fcb28d8bcc2a7ec39293f5d5a02ed39bde25a35b8cc9102f007c09a257c043c7e1bb64068a10338f9bf741a02b042150132c502ac941300d32c271196696b77091ba73b4d9c352bb1237445c6ed15e18f4f2aa3bed7ef9b077b1eb5b5c7441b544f8fedbb79544980d5b9c2659f3997fa")]
 [assembly: InternalsVisibleTo("RedisExplorer_UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100830e56aa64e9ac98ba93c882cd59fe0fcb28d8bcc2a7ec39293f5d5a02ed39bde25a35b8cc9102f007c09a257c043c7e1bb64068a10338f9bf741a02b042150132c502ac941300d32c271196696b77091ba73b4d9c352bb1237445c6ed15e18f4f2aa3bed7ef9b077b1eb5b5c7441b544f8fedbb79544980d5b9c2659f3997fa")]
