8a0de86c10f84309212e771ddc2f2d37d499a0d1
Mohsen Kokabi <mohsen.kokabi@didata.com.au>
2015-05-26 15:55:18 +1000

    Adding the List and Hash to supported types. Adding editing panel (not completed)

--- RedisExplorer/DataTypes/RedisData.cs
+++ RedisExplorer/DataTypes/RedisData.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace DimensionData.RedisExplorer
 {
+	using StackExchange.Redis;
+
 	public class RedisData
 	{
 		public string Key
@@ -14,7 +16,25 @@ namespace DimensionData.RedisExplorer
 			set;
 		}
 
-		public string Value
+		public RedisValue Value
+		{
+			get;
+			set;
+		}
+
+		public RedisValue[] Values
+		{
+			get;
+			set;
+		}
+
+		public HashEntry[] Hash
+		{
+			get;
+			set;
+		}
+
+		public RedisType Type
 		{
 			get;
 			set;
@@ -24,10 +44,59 @@ namespace DimensionData.RedisExplorer
 		{
 		}
 
-		public RedisData(string key, string value)
+		private RedisData(string key, RedisType type)
 		{
 			this.Key = key;
+			this.Type = type;
+		}
+
+		public RedisData(string key, RedisValue value)
+			: this(key, RedisType.String)
+		{
 			this.Value = value;
+		}
+
+		public RedisData(string key, RedisValue[] values)
+			: this(key, RedisType.List)
+		{
+			this.Values = values;
+		}
+
+		public RedisData(string key, HashEntry[] hash)
+			: this(key, RedisType.Hash)
+		{
+			this.Hash = hash;
+		}
+
+		/// <summary>
+		/// Returns a string that represents the current object.
+		/// </summary>
+		/// <returns>
+		/// A string that represents the current object.
+		/// </returns>
+		public override string ToString()
+		{
+			return Text;
+		}
+
+		public string Text
+		{
+			get
+			{
+				if (Type == RedisType.String)
+				{
+					return this.Value;
+				}
+				if (Type == RedisType.List)
+				{
+					return String.Join(",", this.Values.ToStringArray());
+				}
+				if (Type == RedisType.Hash)
+				{
+					return String.Join(",", this.Hash.Select(item => string.Format("{0}:{1}", item.Name, item.Value)).ToArray());
+				}
+				return base.ToString();
+			}
 		}
 	}
 }
--- RedisExplorer/ViewModel/RedisExplorerViewModel.cs
+++ RedisExplorer/ViewModel/RedisExplorerViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Net;
@@ -10,6 +10,8 @@ using System.Windows.Input;
 
 namespace DimensionData.RedisExplorer.ViewModel
 {
+
+
 	using GalaSoft.MvvmLight.Command;
 
 	using StackExchange.Redis;
@@ -17,16 +19,34 @@ namespace DimensionData.RedisExplorer.ViewModel
 	public class RedisExplorerViewModel
 		: INotifyPropertyChanged
 	{
+		#region Private fields
 		string redisUrl;
 
+		bool editMode;
+
+		RedisData selectedItem;
+
 		ICommand connectCommand;
-		
+
+		ICommand saveCommand;
+
+		ICommand cancelCommand;
+
+		ICommand valueChangedCommand;
+
+		ICommand rowChangedCommand;
+
+		ICommand gridDoubleClickCommand;
+		#endregion
+
 		public RedisExplorerViewModel()
 		{
-			KeyValueCollection = new ObservableCollection<RedisData>();
+			KeyValueCollection = new RedisDataCollection();
 			this.redisUrl = "localhost:6379";
+			this.editMode = false;
 		}
 
+		#region Commands
 		public ICommand ConnectCommand
 		{
 			get
@@ -35,10 +55,68 @@ namespace DimensionData.RedisExplorer.ViewModel
 			}
 		}
 
+		public ICommand SaveCommand
+		{
+			get
+			{
+				return saveCommand ?? (saveCommand = new RelayCommand(() => { Save(); }));
+			}
+		}
+
+		public ICommand CancelCommand
+		{
+			get
+			{
+				return cancelCommand ?? (cancelCommand = new RelayCommand(() => { Cancel(); }));
+			}
+		}
+
+		public ICommand ValueChangedCommand
+		{
+			get
+			{
+				return valueChangedCommand ?? (valueChangedCommand = new RelayCommand<string>(textChanged => { ValueChanged(textChanged); }));
+			}
+		}
+
+		public ICommand RowChangedCommand
+		{
+			get
+			{
+				return rowChangedCommand ?? (rowChangedCommand = new RelayCommand<IList>(items => { RowChanged(items); }));
+			}
+		}
+
+		public ICommand GridDoubleClickCommand
+		{
+			get
+			{
+				return gridDoubleClickCommand ?? (gridDoubleClickCommand = new RelayCommand<RedisData>(selectedItem => { SwitchToEditMode(selectedItem); }));
+			}
+		} 
+		#endregion
+
+		#region Public properties
+		public RedisData SelectedItem
+		{
+			get
+			{
+				return selectedItem;
+			}
+			set
+			{
+				if (value != selectedItem)
+				{
+					this.selectedItem = value;
+					this.OnPropertyChanged("SelectedItem");
+				}
+			}
+		}
+
 		/// <summary>
 		/// Gets or sets the key value collection.
 		/// </summary>
-		public ObservableCollection<RedisData> KeyValueCollection
+		public RedisDataCollection KeyValueCollection
 		{
 			get;
 			set;
@@ -46,6 +124,10 @@ namespace DimensionData.RedisExplorer.ViewModel
 
 		public string RedisUrl
 		{
+			get
+			{
+				return this.redisUrl;
+			}
 			set
 			{
 				if (value != this.redisUrl)
@@ -54,12 +136,43 @@ namespace DimensionData.RedisExplorer.ViewModel
 					this.OnPropertyChanged("RedisUrl");
 				}
 			}
+		}
+
+		public bool EditMode
+		{
 			get
 			{
-				return this.redisUrl; 
+				return this.editMode;
+			}
+			set
+			{
+				if (value != this.editMode)
+				{
+					this.editMode = value;
+					this.OnPropertyChanged("EditMode");
+					this.OnPropertyChanged("BrowseMode");
+					this.OnPropertyChanged("EditPanelVisibility");
+				}
 			}
 		}
-		
+
+		public Visibility EditPanelVisibility
+		{
+			get
+			{
+				return editMode ? Visibility.Visible : Visibility.Collapsed;
+			}
+		}
+
+		public bool BrowseMode
+		{
+			get
+			{
+				return !this.EditMode;
+			}
+		} 
+		#endregion
+
 		public void Connect()
 		{
 			try
@@ -70,13 +183,74 @@ namespace DimensionData.RedisExplorer.ViewModel
 				IEnumerable<RedisKey> keys = redisServer.Keys(pageSize: 10);
 				var redisDatabase = redisConnection.GetDatabase();
 				KeyValueCollection.Clear();
-				keys.ToList().ForEach(key => KeyValueCollection.Add(new RedisData(key, redisDatabase.StringGet(key))));
+				keys.ToList().ForEach(key =>
+					{
+						try
+						{
+							if (redisDatabase.KeyType(key) == RedisType.String)
+							{
+								KeyValueCollection.Add(new RedisData(key, redisDatabase.StringGet(key)));
+							}
+							if (redisDatabase.KeyType(key) == RedisType.List)
+							{
+								KeyValueCollection.Add(new RedisData(key, redisDatabase.ListRange(key)));
+							}
+							if (redisDatabase.KeyType(key) == RedisType.Hash)
+							{
+								KeyValueCollection.Add(new RedisData(key, redisDatabase.HashGetAll(key)));
+							}
+						}
+						catch (Exception ex)
+						{
+							KeyValueCollection.Add(new RedisData(key, ex.Message));
+						}
+					});
+				SelectedItem = KeyValueCollection.FirstOrDefault();
 			}
 			catch (Exception ex)
 			{
 				MessageBox.Show(string.Format("Can not connect to {0}. {1}", this.RedisUrl,  ex.Message),
 				   "Redis Explorer");
 			}
+		}
+
+		public void SwitchToEditMode(RedisData redisData)
+		{
+			this.EditMode = true;
+			MessageBox.Show("SwitchToEditMode " + redisData.Key + " " + redisData.Value);
+		}
+		
+		public void Save()
+		{
+			MessageBox.Show("Save " + selectedItem.Key + " " + selectedItem.Value);
+			this.EditMode = false;
+		}
+
+		public void Cancel()
+		{
+			this.EditMode = false;
+		}
+
+		public void ValueChanged(string textChanged)
+		{
+			try
+			{
+				// MessageBox.Show("ValueChanged " + selectedItem.Key + " " + textChanged);
+				KeyValueCollection.FirstOrDefault(item => item.Key == selectedItem.Key).Value = textChanged;
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Can not write the value back to the Redis");
+			}
+		}
+
+		public void RowChanged(IList selectedCellsChangedEventArgs)
+		{
+			if (selectedCellsChangedEventArgs.Count == 0)
+			{
+				return;
+			}
+			this.SelectedItem = (RedisData)selectedCellsChangedEventArgs[0];
 		}
 
 		public event PropertyChangedEventHandler PropertyChanged;
