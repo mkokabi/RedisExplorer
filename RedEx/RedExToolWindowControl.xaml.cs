601298b0e1276149daee6f606d735c07fab78e51
Mohsen Kokabi <mkokabi@yahoo.com>
2020-03-14 22:43:00 +1100

    Adding new RedEx project.

---
+++ RedEx/RedExToolWindowControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace RedEx
+{
+    using RedisExplorer.UserControl;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for RedExToolWindowControl.
+    /// </summary>
+    public partial class RedExToolWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RedExToolWindowControl"/> class.
+        /// </summary>
+        public RedExToolWindowControl()
+        {
+            this.InitializeComponent();
+
+            this.Content = new Explorer();
+        }
+    }
+}
