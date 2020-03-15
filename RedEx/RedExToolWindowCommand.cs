3c146c1372371abda45c632bec0833373f737597
Mohsen Kokabi <mkokabi@yahoo.com>
2020-03-15 15:25:43 +1100

    Changing the ToolWindow back from async to normal.

--- RedEx/RedExToolWindowCommand.cs
+++ RedEx/RedExToolWindowCommand.cs
@@ -71,11 +71,11 @@ namespace RedEx
         /// <param name="package">Owner package, not null.</param>
         public static async Task InitializeAsync(AsyncPackage package)
         {
-            // Switch to the main thread - the call to AddCommand in RedExToolWindowCommand's constructor requires
+            // Switch to the main thread - the call to AddCommand in RedExSyncToolWindowCommand's constructor requires
             // the UI thread.
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
-            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
             Instance = new RedExToolWindowCommand(package, commandService);
         }
 
@@ -86,14 +86,19 @@ namespace RedEx
         /// <param name="e">The event args.</param>
         private void Execute(object sender, EventArgs e)
         {
-            this.package.JoinableTaskFactory.RunAsync(async delegate
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // Get the instance number 0 of this tool window. This window is single instance so this instance
+            // is actually the only one.
+            // The last flag is set to true so that if the tool window does not exists it will be created.
+            ToolWindowPane window = this.package.FindToolWindow(typeof(RedExToolWindow), 0, true);
+            if ((null == window) || (null == window.Frame))
             {
-                ToolWindowPane window = await this.package.ShowToolWindowAsync(typeof(RedExToolWindow), 0, true, this.package.DisposalToken);
-                if ((null == window) || (null == window.Frame))
-                {
-                    throw new NotSupportedException("Cannot create tool window");
-                }
-            });
+                throw new NotSupportedException("Cannot create tool window");
+            }
+
+            IVsWindowFrame windowFrame = (IVsWindowFrame)window.Frame;
+            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
         }
     }
 }
